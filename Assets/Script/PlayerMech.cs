08d9210f65d210e669ee1e8da867dab473c4bf26
Siddharth-732 <siddharthpn2003@gmail.com>
2025-03-31 01:44:56 +0530

    All movement and Animation
    This commit consist of all the movements and the animation for the player model.

--- Assets/Script/PlayerMech.cs
+++ Assets/Script/PlayerMech.cs
@@ -3,29 +3,36 @@ using UnityEngine;
 public class PlayerMech : MonoBehaviour
 {
     private Rigidbody rb;
-
+    //DECLARED ALL THE PRIVATE VARIABLES
     [SerializeField] private float playerSpeedWalk;
     [SerializeField] private float playerSpeedRun = 15f;
     [SerializeField] private float playerJumpForce = 5f;
     [SerializeField] private float rotationSpeed = 10f;
-    private bool IsRunning;
-    private Animator animator;
-    private bool groundCollider;
 
+    //Bool for the Runnign animation
+    private bool isRunning;
+    private Animator animator;
+    
+    //For the groundcollider tag
+    private bool groundCollider;
+    
+    //To fetch the rigidbody component and initiate the animation as soon as the scene starts
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
     }
-
+    
+    // ground collider
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
+            animator.SetInteger("nowjump", 0);
             groundCollider = true;
         }
     }
-
+    // ground collider
     private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
@@ -33,22 +40,31 @@ public class PlayerMech : MonoBehaviour
             groundCollider = false;
         }
     }
-
+    
+    //deals the movement aswell as the rotation of the player
+    //also the conditions for the animation
     void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float horizontal = Input.GetAxis("Horizontal");   //horizontal input
+        float vertical = Input.GetAxis("Vertical");         //vertical input
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);   //bool to check if running
 
-        float speed = isRunning ? playerSpeedRun : playerSpeedWalk;
-        Vector3 movement = new Vector3(horizontal, 0, vertical).normalized;
+        float speed = isRunning ? playerSpeedRun : playerSpeedWalk;    //adjust what speed the player moves
+        Vector3 movement = new Vector3(horizontal, 0, vertical).normalized; 
 
+        //rotation + animation
         if (movement.magnitude > 0)
         {
             animator.SetBool("WalkStart", true);
             if (speed == playerSpeedRun)
             {
-                animator.SetBool("IsRunning", false);
+                animator.SetBool("WalkStart", false);
+                animator.SetBool("stayIdle", false);
+                animator.SetInteger("Running", 10);
+            }
+            else
+            {
+                animator.SetInteger("Running", 0);
             }
             rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
             Quaternion targetRotation = Quaternion.LookRotation(movement);
@@ -56,16 +72,18 @@ public class PlayerMech : MonoBehaviour
         }
         else
         {
-
+            animator.SetInteger("Running", 0);
             animator.SetBool("WalkStart", false);
+            animator.SetBool("stayIdle", true);
         }
     }
-
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && groundCollider)
         {
             rb.AddForce(Vector3.up * playerJumpForce, ForceMode.Impulse);
+            animator.SetInteger("nowjump", 5);
             groundCollider = false;
         }
     }
